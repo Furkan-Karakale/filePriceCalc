114d576121e6680cfd36c67cc8e3f41df30d1b53
Furkan <furkan.karakale@outlook.com>
2025-09-07 23:38:50 +0300

    importlar duzenlendi

--- filePriceCalc.js
+++ filePriceCalc.js
@@ -1,5 +1,5 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.149.0/";
-import { STLLoader } from "https://cdn.skypack.dev/three@0.149.0/three/examples/jsm/loaders/STLLoader";
+import * as THREE from "three";
+import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
 
 export class PRICECALCULATOR {
   id;
